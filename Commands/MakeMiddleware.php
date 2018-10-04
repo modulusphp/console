d69686a8af3349cee64c3a8ee2d24f674e1bde97
Donald Pakkies <donaldpakkies@gmail.com>
2018-10-04 15:24:10 +0200

    Feat: Create a new Middleware class

---
+++ Commands/MakeMiddleware.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Modulus\Console\Commands;
+
+use AtlantisPHP\Console\Command;
+use Modulus\Console\ModulusCLI;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MakeMiddleware extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'make:middleware {name}';
+
+  /**
+   * The full command description.
+   *
+   * @var string
+   */
+  protected $help = 'This command allows you to create a Application Middleware';
+
+  /**
+   * The descriptions of the console commands.
+   *
+   * @var array
+   */
+  protected $descriptions = [
+    'make:middleware' => 'Create a new Middleware class',
+    'name' => 'The name of the class'
+  ];
+
+  /**
+   * @param InputInterface  $input
+   * @param OutputInterface $output
+   *
+   * @return void
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $name = $input->getArgument('name');
+
+    if ($this->add($name)) {
+      return $output->writeln('<info>Middleware "' . $name . '" has been successfuly created.</info>');
+    }
+
+    return $output->writeln('Middleware "' . $name . '" already exists.');
+  }
+
+  /**
+   * Add new command
+   *
+   * @param  string  $name
+   * @param  string  $class
+   * @return boolean
+   */
+  private function add(string $name) : bool
+  {
+    $middlewares = ModulusCLI::$appdir . 'app' . DIRECTORY_SEPARATOR . 'Http' . DIRECTORY_SEPARATOR . 'Middleware';
+    $middleware = $middlewares . DIRECTORY_SEPARATOR . $name . '.php';
+    $namespace = '';
+
+    if (substr_count($name, '/') > 0) {
+      ModulusCLI::_dir(substr($middleware, 0, strrpos($middleware, DIRECTORY_SEPARATOR)));
+      $namespace = substr($name, 0, strrpos($name, DIRECTORY_SEPARATOR));
+      $name = str_replace($namespace . DIRECTORY_SEPARATOR, '', $name);
+
+      $namespace = '\\' . str_replace('/', '\\', $namespace);
+    }
+
+    ModulusCLI::_dir($middlewares);
+
+    $content = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'Assets' . DIRECTORY_SEPARATOR . 'middleware_template');
+    $content = str_replace('{middleware_name}', $name, $content);
+    $content = str_replace('{namespace}', $namespace, $content);
+
+    if (file_exists($middleware)) {
+      return false;
+    }
+    else {
+      file_put_contents($middleware, $content);
+      return true;
+    }
+  }
+
+}
