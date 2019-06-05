208404cc9baa8b24fb3292d1079574efeda396b0
donald_p <donaldpakkies@gmail.com>
2019-06-05 23:14:27 +0200

    feat: added a new craft command
    creates a new mail class

---
+++ Commands/CraftMail.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Modulus\Console\Commands;
+
+use Modulus\Console\ModulusCLI;
+use AtlantisPHP\Console\Command;
+use Modulus\Scaffolding\Template;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CraftMail extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'craft:mail {name}';
+
+  /**
+   * The full command description.
+   *
+   * @var string
+   */
+  protected $help = 'This command allows you to create a mail';
+
+  /**
+   * The descriptions of the console commands.
+   *
+   * @var array
+   */
+  protected $descriptions = [
+    'craft:mail' => 'Create a new mail',
+    'name' => 'The name of the class'
+  ];
+
+  /**
+   * @param InputInterface  $input
+   * @param OutputInterface $output
+   *
+   * @return void
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $name = $input->getArgument('name');
+
+    if ($this->add($name)) {
+      return $output->writeln('<info>Mail "' . $name . '" has been successfully created.</info>');
+    }
+
+    return $output->writeln('Mail "' . $name . '" already exists.');
+  }
+
+  /**
+   * Add asset
+   *
+   * @param  string  $name
+   * @return boolean
+   */
+  private function add(string $name) : bool
+  {
+    $notifications = ModulusCLI::$appdir . 'app' . DIRECTORY_SEPARATOR . 'Mail';
+    $notification = $notifications . DIRECTORY_SEPARATOR . $name . '.php';
+    $namespace = '';
+
+    if (substr_count($name, '/') > 0) {
+      ModulusCLI::_dir(substr($notification, 0, strrpos($notification, DIRECTORY_SEPARATOR)));
+      $namespace = substr($name, 0, strrpos($name, DIRECTORY_SEPARATOR));
+      $name = str_replace($namespace . DIRECTORY_SEPARATOR, '', $name);
+
+      $namespace = '\\' . str_replace('/', '\\', $namespace);
+    }
+
+    ModulusCLI::_dir($notifications);
+
+    $content = Template::asset('mail_template');
+    $content = str_replace('{mail_name}', $name, $content);
+    $content = str_replace('{namespace}', $namespace, $content);
+
+    if (file_exists($notification)) return false;
+
+    file_put_contents($notification, $content);
+    return true;
+  }
+}
